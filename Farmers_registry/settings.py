9023c762ed75d4c2f651b044e099edc4914ceb1e
Zakaria Said <zajarjafary@gmail.com>
2026-09-20 19:16:52 +0300

    added postgresql db

--- Farmers_registry/settings.py
+++ Farmers_registry/settings.py
@@ -97,12 +97,12 @@ WSGI_APPLICATION = 'Farmers_registry.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 # Password validation
